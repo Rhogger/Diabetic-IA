5949a50f62695e6a902e8c986a9e25cc7b50dbd2
jaislaataides <jaisslaataidess@gmail.com>
2025-03-10 11:27:44 -0300

    :boom: build: projeto iniciado em nuxt

---
+++ frontend/models/diabetes_prediction.ts
@@ -0,0 +1,9 @@
+export interface diabetes_prediction { 
+              gender: number,
+            age: number,
+            hypertension: number,
+            heart_disease: number,
+            bmi: number,
+            hba1c_level: number,
+            blood_glucose_level: number,
+}
